4b7971715d67b15aba473b501c2121ab3731c42b
cwen <cwen@regenstrief.org>
2016-03-27 19:57:39 -0400

    more params in controller and html

--- src/main/java/org/run/ui/controller/MainFileActionController.java
+++ src/main/java/org/run/ui/controller/MainFileActionController.java
@@ -28,36 +28,13 @@ public class MainFileActionController {
 	
 	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
     @ResponseBody
-    public String uploadFileHandler(@RequestParam("name") String name,
-            @RequestParam("file") MultipartFile file) {
+    public String uploadFileHandler(//@RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("colStartX") Integer colStartX,
+            @RequestParam("colEndX") Integer colEndX, 
+            @RequestParam("colY") Integer colY) {
  
-//        if (!file.isEmpty()) {
-////            try {
-////                byte[] bytes = file.getBytes();
-//// 
-////                // Creating the directory to store file
-////                String rootPath = System.getProperty("catalina.home");
-////                File dir = new File(rootPath + File.separator + "tmpFiles");
-////                if (!dir.exists())
-////                    dir.mkdirs();
-//// 
-////                // Create the file on server
-////                File serverFile = new File(dir.getAbsolutePath()
-////                        + File.separator + name);
-////                BufferedOutputStream stream = new BufferedOutputStream(
-////                        new FileOutputStream(serverFile));
-////                stream.write(bytes);
-////                stream.close();
-//// 
-////                return "You successfully uploaded file=" + name;
-////            } catch (Exception e) {
-////                return "You failed to upload " + name + " => " + e.getMessage();
-////            }
-//        	return "You successfully uploaded file=" + name;
-//        } else {
-//            return "You failed to upload " + name
-//                    + " because the file was empty.";
-//        }
+      if (!file.isEmpty()) {
 		CSVReader reader = null;
 		try {
 			reader = new CSVReader(new InputStreamReader(file.getInputStream()));
@@ -97,7 +74,7 @@ public class MainFileActionController {
 					}
 			}
 		}
-
+      }
 		return "greeting";
     }
 
